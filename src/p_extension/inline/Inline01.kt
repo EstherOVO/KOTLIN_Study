512a31da4817f09898923040b8d619f64683457c
EstherOVO <tmejwjd@gmail.com>
2024-05-03 14:27:08 +0900

    Kotlin Inline Function

---
+++ src/p_extension/inline/Inline01.kt
@@ -0,0 +1,15 @@
+package p_extension.inline
+
+// inline 함수
+inline fun Int.multiply(other: Int): Int {
+    return this * other
+}
+
+fun main() {
+
+//  inline 이 아닌 확장 함수는 내부적으로 static 함수를 만들어 호출
+    println(3.multiply(4))
+//  inline 함수는 내부적으로는 함수를 호출하지 않음
+//  호출한 지점의 함수 본문을 그대로 사용
+    println(3 * 4)
+}
