edab6e278c14687ad687980c91849521f5f17a90
EstherOVO <tmejwjd@gmail.com>
2024-05-03 11:18:39 +0900

    Kotlin Sealed Class

--- src/o_data/sealedClass/Sealed02.kt
+++ src/o_data/sealedClass/Sealed02.kt
@@ -2,8 +2,3 @@ package o_data.sealedClass
 
 // 같은 패키지 내에서는 상속 가능
 class Bike : Transport("오토바이", 100)
-
-fun main() {
-
-
-}
---
+++ src/o_data/sealedClass/SealedInterface.kt
@@ -0,0 +1,49 @@
+package o_data.sealedClass
+
+sealed interface Clickable {
+
+    fun click()
+}
+
+sealed interface Focusable {
+
+    fun focus()
+}
+
+class Button : Clickable, Focusable {
+
+    override fun click() {
+        println("버튼이 클릭되었습니다.")
+    }
+
+    override fun focus() {
+        println("버튼이 포커스 되었습니다.")
+    }
+}
+
+class TextField : Focusable {
+
+    override fun focus() {
+        println("텍스트 창이 포커스 되었습니다.")
+    }
+}
+
+fun main() {
+
+    val button = Button()
+    button.click()
+    button.focus()
+
+    val textField = TextField()
+    textField.focus()
+
+    handleFocus(button)
+    handleFocus(textField)
+}
+
+fun handleFocus(focusable: Focusable) {
+    when (focusable) {
+        is Button -> println("버튼 포커싱");
+        is TextField -> println("텍스트필드 포커싱")
+    }
+}
